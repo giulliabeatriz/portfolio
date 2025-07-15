7fc4da5becbb26ff233969415800a563f1c5a146
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-15 16:41:13 -0400

    [add]: inclusão do setamento do tema na utilização de useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/navbar'
 
 
 const App = () => {
+
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div className='container'>
-      <Navbar/>
+    <div className={`container ${theme}`}>
+      <Navbar theme={theme} setTheme ={setTheme}/>
     </div>
   )
 }
