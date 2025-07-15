b0ef0c57e92bf566587b63f453dc6b05a04808bd
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-15 16:40:30 -0400

    [fix]: utilização de useState para setar o comportamento do tema - atrelado a mudança de icones e de cores de fundo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
 import './navbar.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import profile from '../../assets/profile.jpg'
+import ti_light from '../../assets/toggle-icon-light.png'
+import ti_dark from '../../assets/toggle-icon-dark.png'
 
-const Navbar = () => {
+
+const Navbar = ({theme, setTheme}) => {
+
+  const toggle_mode = ()=> {
+    theme == 'light' ? setTheme('dark') : setTheme('light');
+    console.log('clicked');
+  }
+
   return (
-    <div>
-     <nav className="navbar">
+    <div className='py-2 align-items-center'>
+      <nav className='navbar d-flex flex-row align-items-center justify-content-between height-25 rounded p-2 px-lg-5 col-12'>
 
-      <img src=".\src\assets\profile.jpg" alt="Foto de perfil" className='profile'/>
+          <span className='d-flex border col-lg-2'>
+            <img src={profile} alt="Foto de perfil" className='profile rounded-circle'/>
+            <p className="px-2 font-weight-bold">Giullia Beatriz</p>
+          </span>
 
-      <ul className='row'>
-        <li>Home</li>
-        <li>Sobre</li>
-        <li>Projetos</li>
-        <li>Contato</li>
-      </ul>
+            <ul className="text-center border col-lg-4">
+              <li>Home</li>
+              <li>Sobre</li>
+              <li>Projetos</li>
+              <li>Contato</li>
+            </ul>
 
-      <img src="" alt="" className='toggle-icon' />
-     </nav>
-    </div>
+            <img onClick={()=>{toggle_mode()}} src={theme == 'light' ? ti_light : ti_dark} alt="" className='toggle-icon rounded-circle p-2 border'/>
+
+      </nav>       
+    </div>  
   )
 }
 
