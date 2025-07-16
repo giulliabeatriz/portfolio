ea89b2d2a955c8c53ee3f3125f9927472e90375d
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-16 16:29:39 -0400

    [fix]: alteração do toggle icon para um botão, visando acessibilidade e estilização com a pseudo-classe :active

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './navbar.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import profile from '../../assets/profile.jpg'
-import ti_light from '../../assets/toggle-icon-light.png'
-import ti_dark from '../../assets/toggle-icon-dark.png'
+import profile from '../../assets/navbar/profile.jpg'
+import ti_light from '../../assets/navbar/toggle-icon-light.png'
+import ti_dark from '../../assets/navbar/toggle-icon-dark.png'
 
 
 const Navbar = ({theme, setTheme}) => {
@@ -17,19 +17,20 @@ const Navbar = ({theme, setTheme}) => {
     <div className='py-2 align-items-center'>
       <nav className='navbar d-flex flex-row align-items-center justify-content-between height-25 rounded p-2 px-lg-5 col-12'>
 
-          <span className='d-flex border col-lg-2'>
+          <span className='d-flex align-items-center col-lg-2'>
             <img src={profile} alt="Foto de perfil" className='profile rounded-circle'/>
-            <p className="px-2 font-weight-bold">Giullia Beatriz</p>
+            <p className="px-2 m-0 font-weight-bold">Giullia Beatriz</p>
           </span>
 
-            <ul className="text-center border col-lg-4">
+            <ul className="text-center m-0 col-lg-4">
               <li>Home</li>
               <li>Sobre</li>
               <li>Projetos</li>
               <li>Contato</li>
             </ul>
 
-            <img onClick={()=>{toggle_mode()}} src={theme == 'light' ? ti_light : ti_dark} alt="" className='toggle-icon rounded-circle p-2 border'/>
+            <button onClick={toggle_mode} aria-label='Alternar tema' className='toggle-icon rounded-circle p-2 border'>
+              <img src={theme == 'light' ? ti_light : ti_dark} alt="Ícone de alternância de tema" className='rounded-circle'/></button>
 
       </nav>       
     </div>  
