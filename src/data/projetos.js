0426fe5220ffc3270d230349e87d1f3a7acdfc3d
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-29 17:17:10 -0400

    [add]: armazenamento dos projetos

---
+++ src/data/projetos.js
@@ -0,0 +1,24 @@
+const projetos = [
+  {
+    titulo: "🎯 Vivendo de Freela",
+    objetivo: "Desenvolver um app para freelancers, focado em usabilidade e acessibilidade.",
+    descricao:
+      "O 'Vivendo de Freela' foi criado na plataforma Adalo com o intuito de conectar freelancers e clientes. Conta com funcionalidades como login, feed de projetos, favoritos, chat, criação de posts e edição de perfil. O design segue boas práticas de UX/UI, com aplicação de conceitos estudados na disciplina de Sistemas Multimídia.",
+    tecnologias: "Adalo (no-code), UX/UI Design, Colorimetria, Acessibilidade",
+    data: "Agosto de 2024",
+    curso: "Sistemas de Informação – UFMT",
+    imagens: [
+      "/images/VivendoDeFreela/freela1.png",
+      "/images/VivendoDeFreela/freela2.png",
+      "/images/VivendoDeFreela/freela3.png",
+      "/images/VivendoDeFreela/freela4.png",
+      "/images/VivendoDeFreela/freela5.png",
+      "/images/VivendoDeFreela/freela6.png",
+      "/images/VivendoDeFreela/freela7.png",
+      "/images/VivendoDeFreela/freela8.png"
+    ],
+    link: "https://giullia-silvas-team.adalo.com/social-media-app"
+  }
+];
+
+export default projetos;
