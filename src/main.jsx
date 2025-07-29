31771f6029b2e7e2fb048574ab45a6fc39af9fb5
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-29 17:13:06 -0400

    [fix]: alteração do reactDOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
+import React from 'react';
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar/navbar.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App>
-        <Navbar />      
-      </App>
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
