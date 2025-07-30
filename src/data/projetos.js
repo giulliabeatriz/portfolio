8488fd7df5732242560bb1354e8d915e0def026e
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-30 16:41:20 -0400

    [add]: inclusão das informações de um novo card

--- src/data/projetos.js
+++ src/data/projetos.js
@@ -1,12 +1,11 @@
 const projetos = [
   {
-    titulo: "🎯 Vivendo de Freela",
-    objetivo: "Desenvolver um app para freelancers, focado em usabilidade e acessibilidade.",
+    titulo: "🥇 Calculadora do DIFAL",
+    objetivo: "Desenvolver uma ferramenta que auxilie os contribuintes na apuração do Diferencial de Alíquotas sobre o ICMS.",
     descricao:
-      "O 'Vivendo de Freela' foi criado na plataforma Adalo com o intuito de conectar freelancers e clientes. Conta com funcionalidades como login, feed de projetos, favoritos, chat, criação de posts e edição de perfil. O design segue boas práticas de UX/UI, com aplicação de conceitos estudados na disciplina de Sistemas Multimídia.",
-    tecnologias: "Adalo (no-code), UX/UI Design, Colorimetria, Acessibilidade",
-    data: "Agosto de 2024 - desenvolvido para a matéria de Sistemas Multimídias na graduação de Sistemas de Informação -- UFMT",
-    curso: "Sistemas de Informação – UFMT",
+      "A 'Calculadora do DIFAL' é uma ferramenta on-line gratuita que auxilia cidadãos contribuintes e não contribuintes a garantirem a conformidade fiscal. A ferramenta conta com campos de entrada de dados e calcula condicionalmente, retornando o valor da Base de Cálculo, do Diferencial de Alíquotas e (caso haja) do FECEP. ",
+    tecnologias: "Liferay (CMS low-code), HTML, CSS, JavaScript",
+    data: "Junho de 2024 - desenvolvido enquanto estagiária da SEFAZ-MT",
     imagens: [
       "/images/VivendoDeFreela/freela1.png",
       "/images/VivendoDeFreela/freela2.png",
@@ -17,7 +16,29 @@ const projetos = [
       "/images/VivendoDeFreela/freela7.png",
       "/images/VivendoDeFreela/freela8.png"
     ],
-    link: "https://giullia-silvas-team.adalo.com/social-media-app"
+        curiosidade: "A Calculadora do DIFAL foi premiada em 1º lugar no 2º Prêmio de Eficiência e Inovação do Governo de Mato Grosso.",
+    link: "https://www.portaldoconhecimento.mt.gov.br/sobre-diferencial-de-aliquotas",
+    tituloLink: "Acesse as calculadoras"
+  },
+  {
+    titulo: "🎯 Vivendo de Freela",
+    objetivo: "Desenvolver um app para freelancers, focado em usabilidade e acessibilidade.",
+    descricao:
+      "O 'Vivendo de Freela' foi criado na plataforma Adalo com o intuito de conectar freelancers e clientes. Conta com funcionalidades como login, feed de projetos, favoritos, chat, criação de posts e edição de perfil. O design segue boas práticas de UX/UI, com aplicação de conceitos estudados na disciplina de Sistemas Multimídia.",
+    tecnologias: "Adalo (no-code), UX/UI Design, Colorimetria, Acessibilidade",
+    data: "Agosto de 2024 - desenvolvido para a matéria de Sistemas Multimídias na graduação de Sistemas de Informação -- UFMT",
+    imagens: [
+      "/images/VivendoDeFreela/freela1.png",
+      "/images/VivendoDeFreela/freela2.png",
+      "/images/VivendoDeFreela/freela3.png",
+      "/images/VivendoDeFreela/freela4.png",
+      "/images/VivendoDeFreela/freela5.png",
+      "/images/VivendoDeFreela/freela6.png",
+      "/images/VivendoDeFreela/freela7.png",
+      "/images/VivendoDeFreela/freela8.png"
+    ],
+    link: "https://giullia-silvas-team.adalo.com/social-media-app",
+    tituloLink: "Acesse o aplicativo"
   }
 ];
 
