6c2606a37aaa60a9e491e8a41f888261773feee0
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-30 13:07:30 -0400

    [add]: criação de um esquema de cores para um design system, utilizando 3 tons da mesma cor (light, default e dark), trabalhando com um esquema primário, um secundário, um accent e feedbacks

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,32 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': {
+          light: '#836cc4',    
+          DEFAULT: '#6750a4', 
+          dark: '#4d3d7d',    
+        },
+        'secondary': {
+          light: '#e5e7eb',
+          DEFAULT: '#4b5563',
+          dark: '#1f2937',
+        },
+        'accent': {
+          light: '#fde047',
+          DEFAULT: '#facc15',
+          dark: '#eab308',
+        },
+        'feedback': {
+          success: '#22c55e', 
+          error: '#ef4444',     
+          warning: '#f97316', 
+        },
+      },
       fontFamily: {
         'sans': ['Comfortaa', ...defaultTheme.fontFamily.sans],
          'heading': ['"Julius Sans One"', 'sans-serif'],
+        'corpo': ['Junge', 'serif'],
       },
     },
   },
