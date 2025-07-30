ce39a0e3d0a3ae52b855e2b2098683de6d62c8e3
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-30 14:24:54 -0400

    [fix]: alteração da fonte do titulo

--- src/components/Pages/Projetos.jsx
+++ src/components/Pages/Projetos.jsx
@@ -5,7 +5,7 @@ import projetos from "../../data/projetos";
 const Projetos = () => {
   return (
     <div className="p-6 space-y-10 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-bold text-center dark:text-primary-light text-primary-dark mb-6">Meus Projetos</h1>
+      <h1 className="text-3xl font-heading font-bold text-center dark:text-primary-light text-primary-dark mb-6">Meus Projetos</h1>
       {projetos.map((proj, index) => (
         <ProjetoCard key={index} {...proj} />
       ))}
