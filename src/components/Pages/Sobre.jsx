7fec428c508c605d9f6f02ea6dc73d5c8feef9a6
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-30 16:40:57 -0400

    [add]: inclusão dos parágrafos de texto

--- src/components/Pages/Sobre.jsx
+++ src/components/Pages/Sobre.jsx
@@ -5,20 +5,22 @@ import './sobre.css';
 const Sobre = () => {
   return (
     <div className="p-8 md:p-16">
-      <h1 className="font-heading text-center text-5xl text-primary mb-8">Sobre Mim</h1>
+      <h1 className="font-heading text-center text-5xl dark:text-primary-light text-primary mb-8">Sobre Mim</h1>
 
-      <div className="text-lg items-start text-secondary-dark dark:text-secondary-light leading-relaxed">
+      <div className="text-lg items-start border justify-center text-secondary-dark dark:text-secondary-light">
         <img 
           src={imgColacao} 
           alt="Giullia Beatriz" 
-          className="float-right w-1/2 md:w-1/3 mr-6 mb-4 shape-image" 
+          className="float-left w-1/2 md:w-1/3 mr-6 h-auto shape-image" 
         />
         
-        <p className="mt-2 mb-4">
-          teste
+        <p className="mt-2 mb-0">
+          Olá, eu sou a Giullia! 👋 
         </p>
-        <p className="mb-4">
-          teste
+        <p className="mb-4 font-corpo">
+          Tenho 21 anos e sou Bacharel em Sistemas de Informação pela Universidade Federal de Mato Grosso (UFMT). 
+          Sou natural de Cuiabá-MT (de chapa e cruz!) e uma das minhas características mais imperceptíveis é que sou Corinthiana roxa e amo escutar música, principalmente o gênero rap/hip-hop e as suas vertentes. 
+          Sou movida pela curiosidade e pelo desejo constante de aprender, sendo apaixonada por criar experiências digitais que realmente façam a diferença. 
         </p>
         <p>
           teste
