0845abe9ff18d22bdf97cc4ceacc639ab0e42307
Giullia <giulliabeatrizrsilva@gmail.com>
2025-07-31 13:39:28 -0400

    [fix]: alteração das imagens, estavam invertidas

--- src/data/projetos.js
+++ src/data/projetos.js
@@ -6,15 +6,13 @@ const projetos = [
       "A 'Calculadora do DIFAL' é uma ferramenta on-line gratuita que auxilia cidadãos contribuintes e não contribuintes a garantirem a conformidade fiscal. A ferramenta conta com campos de entrada de dados e calcula condicionalmente, retornando o valor da Base de Cálculo, do Diferencial de Alíquotas e (caso haja) do FECEP. ",
     tecnologias: "Liferay (CMS low-code), HTML, CSS, JavaScript",
     data: "Junho de 2024 - desenvolvido enquanto estagiária da SEFAZ-MT",
-    imagens: [
-      "/images/VivendoDeFreela/freela1.png",
-      "/images/VivendoDeFreela/freela2.png",
-      "/images/VivendoDeFreela/freela3.png",
-      "/images/VivendoDeFreela/freela4.png",
-      "/images/VivendoDeFreela/freela5.png",
-      "/images/VivendoDeFreela/freela6.png",
-      "/images/VivendoDeFreela/freela7.png",
-      "/images/VivendoDeFreela/freela8.png"
+    imagens: [     
+      "/images/calculadoraDifal/calc1.jpeg",
+      "/images/calculadoraDifal/calc3.png",
+      "/images/calculadoraDifal/calc4.png",
+      "/images/calculadoraDifal/calc5.jpeg",
+      "/images/calculadoraDifal/calc5.png",
+      "/images/calculadoraDifal/calc6.png", 
     ],
         curiosidade: "A Calculadora do DIFAL foi premiada em 1º lugar no 2º Prêmio de Eficiência e Inovação do Governo de Mato Grosso.",
     link: "https://www.portaldoconhecimento.mt.gov.br/sobre-diferencial-de-aliquotas",
@@ -28,12 +26,14 @@ const projetos = [
     tecnologias: "Adalo (no-code), UX/UI Design, Colorimetria, Acessibilidade",
     data: "Agosto de 2024 - desenvolvido para a matéria de Sistemas Multimídias na graduação de Sistemas de Informação -- UFMT",
     imagens: [
-      "/images/calculadoraDifal/calc1.jpeg",
-      "/images/calculadoraDifal/calc3.png",
-      "/images/calculadoraDifal/calc4.png",
-      "/images/calculadoraDifal/calc5.jpeg",
-      "/images/calculadoraDifal/calc5.png",
-      "/images/calculadoraDifal/calc6.png",      
+      "/images/VivendoDeFreela/freela1.png",
+      "/images/VivendoDeFreela/freela2.png",
+      "/images/VivendoDeFreela/freela3.png",
+      "/images/VivendoDeFreela/freela4.png",
+      "/images/VivendoDeFreela/freela5.png",
+      "/images/VivendoDeFreela/freela6.png",
+      "/images/VivendoDeFreela/freela7.png",
+      "/images/VivendoDeFreela/freela8.png" 
     ],
     link: "https://giullia-silvas-team.adalo.com/social-media-app",
     tituloLink: "Acesse o aplicativo"
